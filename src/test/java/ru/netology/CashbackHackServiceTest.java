bffb7e1d7a40e63d89f8b3671316788b1381fe81
Petrov Maxim <maxim.petrov@1ci.com>
2023-07-17 22:41:34 +0300

    stab test for JUnit4

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -8,9 +8,9 @@ public class CashbackHackServiceTest {
     @org.junit.Test
     public void correctCost(){
         CashbackHackService test = new CashbackHackService();
-        int amount = 999;
+        int amount = 900;
         int actual = test.remain(amount);
-        int expected = 1;
+        int expected = 100;
         assertEquals(expected , actual);
     }
 
