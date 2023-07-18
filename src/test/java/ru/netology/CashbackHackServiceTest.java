9703ccbdbe1a2cdb0a10822611fa13b229998865
Petrov Maxim <maxim.petrov@1ci.com>
2023-07-18 08:02:28 +0300

    add negative test

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -23,4 +23,12 @@ public class CashbackHackServiceTest {
         assertEquals(expected , actual);
     }
 
+    @org.junit.Test
+    public void incorrectCost(){
+        CashbackHackService test = new CashbackHackService();
+        int amount = 1000;
+        int actual = test.remain(amount);
+        int expected = 0;
+        assertEquals(expected , actual);
+    }
 }
